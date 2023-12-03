3be0c826f3d39f9e99d8327e6849f47cc725de89
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2023-12-03 20:33:36 +0100

    Update python.js

--- python.js
+++ python.js
@@ -1,9 +1,20 @@
 function print(value) {
-  console.log(value)
+  console.log(value);
 }
 
-function plus(value1, value2) {
-  const value = value1 + value2
-
-  return value
+function plus(value1, value2) 
+  return value1 + value2;
 }
+
+function minus(value1, value2) {
+  return value1 - value2;
+}
+
+function multiply(value1, value2) }
+  return value1 * value2;
+}
+
+function divide(value1, value2) {
+    return value1 / value2;
+}
+
